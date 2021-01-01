c20ffb382e350faa60006580a933a8b31ffacdc5
Trent Conley <trentconley@gmail.com>
2020-12-31 19:28:44 -0600

    added working-in-progress version of hello world

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+# The following code will serve to act as an introduction to the basic elements of Python 
+# through the creation of a hello world program. 
+
+# Comments are made by entering the hashtag # at the beginning of a line. Anything after the 
+# hashtag will not be read by your computer, so you can use comments to make your code
+# clear to your reader, as I am doing now. 
+
+# Lets start with a simple function that is built into Python, called print
+print "Hello, World!"
+
+print ("Hello, World!")
+
+# Notice how you will get an error on line 9, that is because all functions need a parenthesis 
+# to operate properly. Using this knowledge, put a hashtag # in the begninning of the line that 
+# you think that is wrong so that the computer will ignore it. 
+
+# If you chose right, the computer should have printed Hello World! This is a common test that
+# software engineers will use to make sure that their code is working properly. 
+
+# Now, we are going to use a varable, and print what the variable is. 
+
+x = ""
