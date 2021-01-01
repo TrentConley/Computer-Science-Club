9c72b938477fca834d8e2d4458a73174405cb055
Trent Conley <trentconley@gmail.com>
2020-12-31 19:41:40 -0600

    finished rough draft

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,8 @@
 
 # Comments are made by entering the hashtag # at the beginning of a line. Anything after the 
 # hashtag will not be read by your computer, so you can use comments to make your code
-# clear to your reader, as I am doing now. 
+# clear to your reader, as I am doing now. The first couple programs will be comment-intensive,
+# but as we get better there will be more code and less of my comments. 
 
 # Lets start with a simple function that is built into Python, called print
 print "Hello, World!"
@@ -19,4 +20,22 @@ print ("Hello, World!")
 
 # Now, we are going to use a varable, and print what the variable is. 
 
-x = ""
+x = "Trent"
+print ("Nice to meet you, " + x)
+
+# Try changing the name stored in the variable, Trent, to your name. Make sure the save the
+# program before running it! Notice how when you run the program again, the name Trent changes
+# to the name that you changed Trent to. The variable x stores what is called a string. A 
+# string can be anything from a character (not in java, another programming language, but 
+# we'll keep it simple for now) to a long essay. You can think of a string as stringing together
+# a whole bunch of characters (including but not limited to alphabetical characters!) to create 
+# something larger and hopefully more meaningfull :). 
+
+# now, try and declare a varable (declare just means to set a variable equal to something in this
+# case), and print it!
+# type code below
+
+
+
+# If your program worked, great job! We will continue with this more next lesson (or if everybody
+# is extra speedy, in a couple minutes when everyone finishes!).
