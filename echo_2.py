8fb2d64d8834474e1b801555ac96dad848347e40
Trent Conley <trentconley@gmail.com>
2021-01-14 10:51:12 -0600

    working on echo2

--- echo_2.py
+++ echo_2.py
@@ -1,1 +1,18 @@
-# I have yet to write anything
+# Using what you learned from echo_1, get a user input for 
+# a long sentence. 
+# Enter code below: 
+my_string = 
+
+
+# This is how to get the character of location i of a string
+# The location of a string will always start at 0. 
+i = 0
+my_character = ''
+length = len(string)
+# single quotation marks represent a character. we have set it to nothing for now. 
+
+for i in range (0,length):
+	my_character = my_string[i]
+	print (my_character)
+
+
