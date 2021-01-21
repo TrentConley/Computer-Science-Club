a70ce25d819199fd86c387cfb1adec2a6ff23b9a
Trent Conley <trentconley@gmail.com>
2021-01-21 09:15:31 -0600

    finished shift1

--- Shift_1.py
+++ Shift_1.py
@@ -1,1 +1,24 @@
-Shift_1.py
+# Shift_1.py
+
+# In this program, you will learn how to manpulate user input even more. 
+
+# Get user input:
+
+my_string = 
+
+# get the first character of the string
+
+first_character = 
+
+first_character_plus_one = first_character + 1
+
+print (first_character_plus_one)
+# the line above will shift the letter over 1. 
+# For example, an 'a' will turn into a 'b'. 
+
+# using what you know from echo_2 and above, add two to every
+# letter of a my_string, the user input. 
+# Enter code below:
+
+
+
