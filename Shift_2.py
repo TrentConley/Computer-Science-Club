4fdd9cf8f918461b095acfd0e061aed5b6d95dec
Trent Conley <trentconley@gmail.com>
2021-01-21 14:32:03 -0600

    working on shift2

--- Shift_2.py
+++ Shift_2.py
@@ -2,9 +2,13 @@
 # then you would get something funky. So, we need to learn how to use if statements.
 
 # Get user input:
+# only input lowercase letters for now, we will use capital letters soon
 
 my_string = 
 
+# the following lines will declare
+
 for i in range (0, len(my_string)):
-	if (i > )
+	if (i >= ord('a') and i <= 'z'):
+
 	print 
