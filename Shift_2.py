ef525c132254e5ca8536656b658fca8bf21eff00
Trent Conley <trentconley@gmail.com>
2021-01-21 10:29:58 -0600

    working

--- Shift_2.py
+++ Shift_2.py
@@ -1,1 +1,7 @@
-Shift_2.py
+# You might have noticed that if the first letter was a 'z', in shift_1
+# then you would get something funky. So, we need to learn how to use if statements.
+
+# Get user input:
+
+my_string = 
+
