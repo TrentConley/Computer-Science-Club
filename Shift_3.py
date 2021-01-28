16ab110665c39684fbfa93c6255c20eb50a18ca3
Trent Conley <trentconley@gmail.com>
2021-01-28 13:36:28 -0600

    working on shift3

--- Shift_3.py
+++ Shift_3.py
@@ -1,2 +1,10 @@
 # Shift_3.py
 # We will work with wrapping around, uppercase letters, and more edgecases. 
+
+# Get a user sentence:
+my_sentence = 
+
+
+# get shift, and turn it into an integer
+shift = 
+
