a22478ada769214a47dc5d000e9f1d4dc42b9797
Trent Conley <trentconley@gmail.com>
2021-01-28 13:46:52 -0600

    more edits, added testing.py

--- Shift_3.py
+++ Shift_3.py
@@ -5,9 +5,12 @@
 my_sentence = 
 
 
-# get shift, and turn it into an integer
+# get shift, and turn it into an integer you can also enter a really big number
 
 my_shift = 
+
+if (my_shift >= 26):
+	my_shift = 26%my_shift 
 
 
 # there is a lot of carryover from Shift_2, but it is more compact. 
@@ -15,4 +18,6 @@ for i in range (0, len(my_string)):
 	if (ord(my_sting[i]) >= ord('a') and ord(my_sting[i]) <= ord('z')):
 		# now we know that all of the letters that we will apply the shift to
 		# are inbetween a and z.
+		# the next if statement will allow for 
+		if (ord)
 		
